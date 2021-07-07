c57503f5cf01cae260add03fd8104cbee40795f8
crutchbandicoot <tsheposet@hotmail.com>
2021-07-07 08:41:09 +0200

    reworked the hero section to be a component, arrow buttons are now components

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,48 @@
 import React from "react";
 import "./App.scss";
-import Navbar from "./components/Navbar/Navbar";
 import arrow from "./assets/icons/icon-arrow.svg";
 import left from "./assets/icons/icon-angle-left.svg";
 import right from "./assets/icons/icon-angle-right.svg";
+import Desktop_Slide_1 from "./assets/img/Desktop_Slide_1.jpg";
+import Desktop_Slide_2 from "./assets/img/Desktop_Slide_2.jpg";
+import Desktop_Slide_3 from "./assets/img/Desktop_Slide_3.jpg";
+import Mobile_Slide_1 from "./assets/img/Mobile_Slide_1.jpg";
+import Mobile_Slide_2 from "./assets/img/Mobile_Slide_2.jpg";
+import Mobile_Slide_3 from "./assets/img/Mobile_Slide_3.jpg";
+import { HeroInfo } from "./interfaces/hero.interface";
+import Hero from "./components/Hero/Hero";
 
 function App() {
+  const heroData: HeroInfo[] = [
+    {
+      heading: "Discover innovative ways to decorate",
+      text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+      desktopImg: Desktop_Slide_1,
+      mobileImg: Mobile_Slide_1,
+    },
+    {
+      heading: "We are available all across the globe",
+      text: "With stores all over the world, it's easy for you to find furniture for your home or place of business.  Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+      desktopImg: Desktop_Slide_2,
+      mobileImg: Mobile_Slide_2,
+    },
+    {
+      heading: "Manufactured with the best materials",
+      text: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+      desktopImg: Desktop_Slide_3,
+      mobileImg: Mobile_Slide_3,
+    },
+  ];
   return (
     <div>
-      <div className="row g-0">
-        <div className="col-7">
-          <div className="hero-img">
-            <div className="nav-container">
-              <Navbar />
-            </div>
-          </div>
-        </div>
-        <div style={{ position: "relative" }} className="col-5">
-          <div className="abs-center" style={{ width: "80%" }}>
-            <h2 className="heading">Discover innovative ways to decorate</h2>
-            <p className="text">
-              We provide unmatched quality, comfort, and style for property
-              owners across the country. Our experts combine form and function
-              in bringing your vision to life. Create a room in your own style
-              with our collection and make your property a reflection of you and
-              what you love.
-            </p>
-            <h5 className="shop-now">
-              {/* eslint-disable-next-line */}
-              <a href="#">
-                Shop now
-                <span>
-                  <img src={arrow} alt="" />
-                </span>
-              </a>
-            </h5>
-          </div>
-          <div className="slider">
-            <div className="slider__wrapper">
-              <img className="slider--left" src={left} alt="left arrow" />
-            </div>
-            <div className="slider__wrapper">
-              <img className="slider--right" src={right} alt="right arrow" />
-            </div>
-          </div>
-        </div>
-      </div>
+      {/* Hero section */}
+      <Hero
+        heading={heroData[0].heading}
+        text={heroData[0].text}
+        backgroundImage={heroData[0].desktopImg}
+      />
+
+      {/* About section */}
       <div className="row g-0">
         <div className="col-3">
           <div className="img">
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import "./Hero.scss";
+import { createUseStyles } from "react-jss";
+import arrow from "../../assets/icons/icon-arrow.svg";
+import left from "../../assets/icons/icon-angle-left.svg";
+import right from "../../assets/icons/icon-angle-right.svg";
 
-const Hero = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+import Button from "../Button/Button";
+
+interface Hero {
+  heroImg: string;
+  backgroundImg: any;
+  backgroundSize: string;
 }
 
-export default Hero
+const styles = createUseStyles((theme: any) => ({
+  heroImg: {
+    height: "70vh",
+    backgroundImage: (props: any) => `url(${props.backgroundImage})`,
+    backgroundSize: "100%",
+    backgroundRepeat: "no-repeat",
+  },
+}));
+
+const Hero = (props: any) => {
+  const classes = styles(props);
+
+  return (
+    <div>
+      <div className="row g-0">
+        <div className="col-sm-7">
+          <div className={classes.heroImg}>
+            <div className="nav-container">
+              <Navbar />
+            </div>
+          </div>
+        </div>
+        <div style={{ position: "relative" }} className="col-sm-5">
+          <div style={{ width: "85%" }}  className="abs-center">
+            <h2>{props.heading}</h2>
+            <p>{props.text}</p>
+            <a href="#">
+              Shop now
+              <span>
+                <img src={arrow} alt="" />
+              </span>
+            </a>
+          </div>
+          <div className="slider">
+            <Button img={left}/>
+            <Button img={right}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
