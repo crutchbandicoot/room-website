555183aae2b417f3564305598de954c56b3f90c4
crutchbandicoot <tsheposet@hotmail.com>
2021-07-08 16:30:04 +0200

    fixed css styling from bleeding into other components, added gsap animation library, removed unecesaary code in the hero component, moved data from app component to hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,14 @@
 import React from "react";
 import "./App.scss";
-import arrow from "./assets/icons/icon-arrow.svg";
-import left from "./assets/icons/icon-angle-left.svg";
-import right from "./assets/icons/icon-angle-right.svg";
-import Desktop_Slide_1 from "./assets/img/Desktop_Slide_1.jpg";
-import Desktop_Slide_2 from "./assets/img/Desktop_Slide_2.jpg";
-import Desktop_Slide_3 from "./assets/img/Desktop_Slide_3.jpg";
-import Mobile_Slide_1 from "./assets/img/Mobile_Slide_1.jpg";
-import Mobile_Slide_2 from "./assets/img/Mobile_Slide_2.jpg";
-import Mobile_Slide_3 from "./assets/img/Mobile_Slide_3.jpg";
-import { HeroInfo } from "./interfaces/hero.interface";
+
 import Hero from "./components/Hero/Hero";
 
 function App() {
-  const heroData: HeroInfo[] = [
-    {
-      heading: "Discover innovative ways to decorate",
-      text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
-      desktopImg: Desktop_Slide_1,
-      mobileImg: Mobile_Slide_1,
-    },
-    {
-      heading: "We are available all across the globe",
-      text: "With stores all over the world, it's easy for you to find furniture for your home or place of business.  Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
-      desktopImg: Desktop_Slide_2,
-      mobileImg: Mobile_Slide_2,
-    },
-    {
-      heading: "Manufactured with the best materials",
-      text: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
-      desktopImg: Desktop_Slide_3,
-      mobileImg: Mobile_Slide_3,
-    },
-  ];
+  
   return (
     <div>
       {/* Hero section */}
-      <Hero
-        heading={heroData[0].heading}
-        text={heroData[0].text}
-        backgroundImage={heroData[0].desktopImg}
-      />
+      <Hero/>
 
       {/* About section */}
       {/* <div style={{paddingTop: "10rem"}} className="row g-0">
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,42 @@
 import React from "react";
-import Navbar from "../Navbar/Navbar";
+
 import "./Hero.scss";
 import { createUseStyles } from "react-jss";
+import { HeroInfo } from "../../interfaces/hero.interface";
+
 import arrow from "../../assets/icons/icon-arrow.svg";
 import left from "../../assets/icons/icon-angle-left.svg";
 import right from "../../assets/icons/icon-angle-right.svg";
+import Desktop_Slide_1 from "../../assets/img/Desktop_Slide_1.jpg";
+import Desktop_Slide_2 from "../../assets/img/Desktop_Slide_2.jpg";
+import Desktop_Slide_3 from "../../assets/img/Desktop_Slide_3.jpg";
+import Mobile_Slide_1 from "../../assets/img/Mobile_Slide_1.jpg";
+import Mobile_Slide_2 from "../../assets/img/Mobile_Slide_2.jpg";
+import Mobile_Slide_3 from "../../assets/img/Mobile_Slide_3.jpg";
 
+import Navbar from "../Navbar/Navbar";
 import Button from "../Button/Button";
+
+const heroData: HeroInfo[] = [
+  {
+    heading: "Discover innovative ways to decorate",
+    text: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+    desktopImg: Desktop_Slide_1,
+    mobileImg: Mobile_Slide_1,
+  },
+  {
+    heading: "We are available all across the globe",
+    text: "With stores all over the world, it's easy for you to find furniture for your home or place of business.  Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+    desktopImg: Desktop_Slide_2,
+    mobileImg: Mobile_Slide_2,
+  },
+  {
+    heading: "Manufactured with the best materials",
+    text: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+    desktopImg: Desktop_Slide_3,
+    mobileImg: Mobile_Slide_3,
+  },
+];
 
 interface Hero {
   heroImg: string;
@@ -17,7 +47,7 @@ interface Hero {
 const styles = createUseStyles((theme: any) => ({
   heroImg: {
     height: "70vh",
-    backgroundImage: (props: any) => `url(${props.backgroundImage})`,
+    backgroundImage: `url(${heroData[0].desktopImg})`,
     backgroundSize: "100%",
     backgroundRepeat: "no-repeat",
   },
@@ -32,9 +62,9 @@ const changeSlide = () =>{
   console.log('change slide clicked');
 }
 
-const Hero = (props: any) => {
+const Hero = () => {
 
-  const classes = styles(props);
+  const classes = styles();
 
   return (
     <div>
@@ -48,8 +78,8 @@ const Hero = (props: any) => {
         </div>
         <div style={{ position: "relative" }} className="col-sm-5">
           <div className="abs-center">
-            <h2>{props.heading}</h2>
-            <p>{props.text}</p>
+            <h2>{heroData[0].heading}</h2>
+            <p>{heroData[0].text}</p>
             <a href="#">
               Shop now
               <span>
